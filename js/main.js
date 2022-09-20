9e44722b919202e7f9f426bcbcb83eb9b63ec911
MhmodMd1 <abo.bimo.1999@gmail.com>
2022-09-21 02:30:42 +0300

    Stats Increase Number

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ window.onscroll = function () {
         });
     }
 }
+
+
+let nums = document.querySelectorAll(".stats .number")
+let statsSection = document.querySelector(".stats");
+let started = false // Function Started ? No
+
+window.onscroll = function () {
+    if (window.scrollY >= statsSection.offsetTop - 300) {
+        if (!started) {
+            nums.forEach((num) => startCount(num))
+        }
+        started = true
+    }
+}
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count)
+        }
+    }, 2000 / goal)
+}
+console.log(statsSection)
