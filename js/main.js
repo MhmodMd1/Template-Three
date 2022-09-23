04790a9282eb3dadf87092c672902fdb3834fc3b
MhmodMd1 <abo.bimo.1999@gmail.com>
2022-09-24 01:55:37 +0300

    Countdown Timer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 console.log("hello world")
 
+// The End Of The Year Date
+// 1000 milliseconds = 1 Second
+
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+    let dateNow = new Date().getTime();
+
+    // Find The Date Difference Between Now And Countdown Date
+    let dateDiff = countDownDate - dateNow;
+
+    // Get Time Units
+    // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+    if (dateDiff < 0) {
+        clearInterval(counter);
+    }
+}, 1000);
 
 
 let progressSpans = document.querySelectorAll(".progress span");
 let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number")
+let statsSection = document.querySelector(".stats");
+let started = false // Function Started ? No
 
 window.onscroll = function () {
     // Skills Animate Width
@@ -12,14 +45,6 @@ window.onscroll = function () {
             span.style.width = span.dataset.width;
         });
     }
-}
-
-
-let nums = document.querySelectorAll(".stats .number")
-let statsSection = document.querySelector(".stats");
-let started = false // Function Started ? No
-
-window.onscroll = function () {
     if (window.scrollY >= statsSection.offsetTop - 300) {
         if (!started) {
             nums.forEach((num) => startCount(num))
@@ -27,6 +52,7 @@ window.onscroll = function () {
         started = true
     }
 }
+
 
 function startCount(el) {
     let goal = el.dataset.goal;
